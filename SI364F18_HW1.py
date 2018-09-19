98b9b565101db74433351ce509635332b18b594a
Bradley Scharf <scharfb@umich.edu>
2018-09-18 22:14:20 -0400

    completed homework

--- SI364F18_HW1.py
+++ SI364F18_HW1.py
@@ -1,3 +1,4 @@
+#Bradley Scharf
 ## HW 1
 ## SI 364 F18
 ## 1000 points
@@ -6,22 +7,30 @@
 
 ## List below here, in a comment/comments, the people you worked with on this assignment AND any resources you used to find code (50 point deduction for not doing so). If none, write "None".
 
-
+#None
 
 ## [PROBLEM 1] - 150 points
 ## Below is code for one of the simplest possible Flask applications. Edit the code so that once you run this application locally and go to the URL 'http://localhost:5000/class', you see a page that says "Welcome to SI 364!"
 
-from flask import Flask
+from flask import Flask, render_template, request
+import requests
+import json
 app = Flask(__name__)
 app.debug = True
 
-@app.route('/')
+@app.route('/class')
 def hello_to_you():
-    return 'Hello!'
+    return 'Welcome to SI 364!'
+
+@app.route('/movie/<movie_name>')
+def movie(movie_name):
+	base_url = "https://itunes.apple.com/search?"
+	params_diction = {"term": movie_name, "media": "movie"}
+	resp = requests.get(base_url, params=params_diction)
+	text = resp.text
+	return text
 
 
-if __name__ == '__main__':
-    app.run()
 
 
 ## [PROBLEM 2] - 250 points
@@ -45,6 +54,30 @@ if __name__ == '__main__':
 ## Once you enter a number and submit it to the form, you should then see a web page that says "Double your favorite number is <number>". For example, if you enter 2 into the form, you should then see a page that says "Double your favorite number is 4". Careful about types in your Python code!
 ## You can assume a user will always enter a number only.
 
+@app.route('/question')
+def double_fav_num():
+    html_form = '''
+    <html>
+    <body>
+    <form method="GET" action="http://localhost:5000/answer">
+        <label>Enter your favorite number:
+            <input name="Favorite_Number" value="0" type="number">
+        </label></br>
+        <input type="submit" value="Submit">
+    </form>
+    </body>
+    </html>
+    '''
+    return html_form
+
+@app.route('/answer', methods = ['GET', 'POST'])
+def result_doubled():
+    if request.method == 'GET':
+        number = request.args.get('Favorite_Number')
+        number_doubled = str(int(number)*2)
+        return "Double your favorite number is {}".format(number_doubled)
+
+
 
 ## [PROBLEM 4] - 350 points
 
@@ -65,3 +98,47 @@ if __name__ == '__main__':
 # You can assume that a user will give you the type of input/response you expect in your form; you do not need to handle errors or user confusion. (e.g. if your form asks for a name, you can assume a user will type a reasonable name; if your form asks for a number, you can assume a user will type a reasonable number; if your form asks the user to select a checkbox, you can assume they will do that.)
 
 # Points will be assigned for each specification in the problem.
+
+@app.route('/problem4form', methods = ['GET', 'POST'])
+def books():
+    html_form = '''
+    <html>
+    <body>
+    <form method="POST" action="/problem4form">
+        <label>Enter your Zip Code to get the current weather:
+            <input name="Zip_Code" value="" type="text">
+        </label></br>
+        <label>Metrics:
+            <input type="checkbox" name="tempc" value="celcius">Celcius(C)
+            <input type="checkbox" name="tempf" value="fahrenheit">Fahrenheit(F)<br>
+        </label></br>
+        <input type="submit" value="Submit">
+    </form>
+    </body>
+    </html>
+    '''
+    if request.method == 'POST':
+    	zipcode = request.form.get('Zip_Code')
+    	apikey = "780d91df19062e7556708d6d283ff296"
+    	base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    	params_diction = {"zip": zipcode, "APPID": apikey}
+    	resp = requests.get(base_url, params=params_diction)
+    	text = resp.text
+    	output = json.loads(text)
+    	temp = output['main']['temp']
+    	description = output['weather'][0]['description']
+    	print(request.form.get('tempc'))
+    	print(request.form.get('tempf'))
+    	if request.values.get('tempc') == "celcius":
+    		converted_temp_c = (int(temp)/10)
+    		rounded_temp_c = (str(converted_temp_c)[0:5])
+    		html_form = html_form + "Currently the weather outside in your area is " + str(rounded_temp_c)+"°C "+"with " + description + "<br>"
+    	if request.values.get('tempf') == "fahrenheit":
+    		converted_temp_f = ((int(temp)/10) * (9/5)) + 32
+    		rounded_temp_f = (str(converted_temp_f)[0:5])
+    		html_form = html_form + "Currently the weather outside in your area is " + str(rounded_temp_f)+"°F "+"with " + description + "<br>"
+    return html_form
+
+if __name__ == '__main__':
+    app.run()
+
